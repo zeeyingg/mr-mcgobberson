e6db2dc691cc537dff667f7b1d3811c4e22ddb79
Ziying Jian <zjian30@stuy.edu>
2022-09-22 09:31:26 -0400

    updated

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -9,7 +9,8 @@ time spent: <elapsed time in hours, rounded to nearest tenth>
 '''
 DISCO:
 * To import a statement, use from... import...
-* randint() helps you 
+* randint() is an imported function to generate a random integer
+* The conditions for randint() are both inclusive
 
 QCC:
 * How do we get the full names of all thinkerens in all periods?
@@ -23,6 +24,11 @@ krewes = {2:['a', 'b', 'c'], 7:['d', 'e', 'f'], 8:['g', 'h', 'i']}
 
 def selectDevo():
     randomCrew = randint(0,2)
-    return randomCrew
+
+    devoList = krewes[randomCrew]
+
+    randomDevo = randint(0,len(devoList))
+    return devoList[randomDevo]
+    # return krewes[randomCrew:randomDevo]
 
 print(selectDevo())
