c2365d0a616c26283430f98dd7f6fb2e49a1a88b
Ziying Jian <zjian30@stuy.edu>
2022-10-05 18:59:10 -0400

    reverted k07 back

--- 07_teardown/app.py
+++ 07_teardown/app.py
@@ -22,9 +22,6 @@ app.run()  # Q5: Where have you seen similar constructs in other languages?
 DISCO:
 * Flask builds on similar object-oriented programming fundamentals as Java and Python
 * Name the file with the same name as the Flask variable in order for it to run, like Java
-* @ is called a decorater -- the entireity of Flask is built on this
-* Running the script opens up a web server on a local network, which we can access through
-clicking on the hyperlink
 
 QCC:
 0.Similar to initializing an instance of a class, learned in Java 
@@ -37,8 +34,4 @@ where __name__ will be printed -- in that file in the file path.
 5.Similar to the way you would call any method, such as main() 
 ...
 INVESTIGATIVE APPROACH:
-* First, we didn't modify the code and just answered the questions with what we knew
-* We tried running the script using "python3" and "flask" for curiousity's sake
-* We found success with python3, but not with flask
-* 
 '''
